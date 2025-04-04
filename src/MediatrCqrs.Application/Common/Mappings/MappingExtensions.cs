f0aec93c0f952798d3c2db47b8a212eb90e02c93
Traiphakh stihhikaew <sirprak1245@gmail.com>
2025-04-04 13:49:58 +0700

    feat(Application::Common): Add mapping extensions, domain event notification, paginated list, result model, and authorization attribute

---
+++ src/MediatrCqrs.Application/Common/Mappings/MappingExtensions.cs
@@ -0,0 +1,12 @@
+
+using MediatrCqrs.Application.Common.Models;
+
+namespace MediatrCqrs.Application.Common.Mappings;
+
+public static class MappingExtensions
+{
+    public static Task<PaginatedList<TDestination>> PaginatedListAsync<TDestination>(this IQueryable<TDestination> queryable, int pageNumber, int pageSize)
+    {
+        return PaginatedList<TDestination>.CreateAsync(queryable, pageNumber, pageSize);
+    }
+}
---
+++ src/MediatrCqrs.Application/Common/Models/DomainEventNotification.cs
@@ -0,0 +1,13 @@
+using MediatR;
+
+namespace MediatrCqrs.Application.Common.Models;
+
+public class DomainEventNotification<TDomainEvent> : INotification where TDomainEvent : DomainEvent
+{
+    public DomainEventNotification(TDomainEvent domainEvent)
+    {
+        DomainEvent = domainEvent;
+    }
+
+    public TDomainEvent DomainEvent { get; }
+}
